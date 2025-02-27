e7695186c51de077185a6efa03d5231b1a6a85b7
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2025-02-27 22:16:34 +0300

    Fourth Commit

--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,7 @@
+import psycopg2
 from django.shortcuts import render
 
-from config.settings import WEATHER_API
+from config.settings import WEATHER_API, DATABASES
 import requests
 
 from weather.forms import WeatherForm
@@ -24,12 +25,43 @@ def get_weather(city):
     return None
 
 
+db = DATABASES
+
+
 def index(request):
+    weather = None
+    error_mes = None
     if request.method == 'POST':
         form = WeatherForm(request.POST)
         if form.is_valid():
             city = form.cleaned_data['city']
             weather = get_weather(city)
+            if weather:
+                with psycopg2.connect(
+                    dbname='weather_monitor',
+                    user='postgres',
+                    password='1111'
+                ) as conn:
+                    with conn.cursor() as cur:
+                        cur.execute('''
+                        CREATE TABLE IF NOT EXISTS weather (
+                        id SERIAL PRIMARY KEY,
+                        city VARCHAR,
+                        temperature FLOAT
+                        );
+                        ''')
+                        conn.commit()
+                        cur.execute(f'''
+                        INSERT INTO weather (city, temperature)
+                        VALUES ('{weather['city']}',
+                        '{weather['current_weather']['temperature']}');
+                        ''')
+                        conn.commit()
+                        cur.close()
+
+            if not weather:
+                error_mes = f"Ошибка: нет такого города как {city}"
     else:
         form = WeatherForm
-    return render(request, 'weather/index.html', {'form': form, 'weather': weather})
+    return render(request, 'weather/index.html', {'form': form, 'weather': weather,
+                                                  'error_mes': error_mes})
