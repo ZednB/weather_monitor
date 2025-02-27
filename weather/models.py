2e918b605a5898ffffee818fbefce8ab50c59815
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2025-02-27 23:09:28 +0300

    Sixth Commit

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Weather(models.Model):
+    city = models.CharField(max_length=100)
+    temperature = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
--- weather/tests.py
+++ weather/tests.py
@@ -6,19 +6,16 @@ from config.settings import WEATHER_API
 from weather.views import get_weather
 
 
-class TestWeather(unittest.TestCase):
+class TestWeather(TestCase):
     def setUp(self):
-        self.city = 'Moscow'
-        self.get_weather = get_weather(self.city)
+        self.city = "Moscow"
 
-    def test_add(self):
-        self.request = f'http://api.openweathermap.org/geo/1.0/direct?q={self.city}&limit=1&appid={WEATHER_API}'
-        api = requests.get(self.request)
-        if len(api.json()) > 0:
-            api2 = api.json()[0]
-            lat, lon = api2['lat'], api2['lon']
-            weather_api = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
-            weather_data = requests.get(weather_api)
-            weather_date = weather_data.json()
-            weather_date['city'] = self.city
-            self.assertIsNotNone(weather_date)
+    def test_get_weather(self):
+        weather = get_weather(self.city)
+        self.assertIsNotNone(weather)
+        self.assertIn("city", weather)
+        self.assertIn("current_weather", weather)
+
+    def test_get_weather_invalid_city(self):
+        weather = get_weather("FakeCity")
+        self.assertIsNone(weather)
